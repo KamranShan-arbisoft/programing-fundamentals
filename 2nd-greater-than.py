28854a7c92eab1851da94bcc23e27bef9bb241ad
HafizKamran <hafizkamran@A006-00389.local>
2021-10-08 00:27:11 +0500

    Selection Assignments

---
+++ 2nd-greater-than.py
@@ -0,0 +1,13 @@
+
+aNum = 10
+bNum = 9
+cNum = 5
+
+if(aNum > bNum and aNum > cNum):
+    print("A is greater than B and C")
+    
+elif(bNum > aNum and bNum > cNum):
+    print("B is greater than A and C")
+        
+elif(cNum > aNum and cNum > bNum):
+    print("C is greater than A and B")
